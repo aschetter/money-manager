f41dcabb6d99269771869c139956dd93bd1dde9d
Andy Schetter <andyschetter@gmail.com>
2014-11-08 00:10:48 -0600

    Refactor code to simplify ID names

--- calendar.js
+++ calendar.js
@@ -1,36 +1,36 @@
 // Inialize radio buttons for transaction type and frequency
-$('#transactionType').buttonset();
+$('#type').buttonset();
 $('#frequency').buttonset();
 
-$("#addTransaction").accordion({
+// Inialize accordian feature for the transaction adding form
+$("#addForm").accordion({
       collapsible: true
 });
 
-// Inialize datepickers to add single and recurring events
-$('#singleDate').datepicker({dateFormat: "yy-mm-dd"});
-$('#recurringStartDate').datepicker({dateFormat: "yy-mm-dd"});
-$('#recurringEndDate').datepicker({dateFormat: "yy-mm-dd"});
+// Inialize datepickers to add a transaction
+$('#addStart').datepicker({dateFormat: "yy-mm-dd"});
+$('#addEnd').datepicker({dateFormat: "yy-mm-dd"});
 
 // Inialize styling for all buttons
 $('button').button();
 
-// Hold event object to pass from event click to dialog form
-var selectedEvent;
+// Hold transaction to use in edit/ delete dialog
+var selected;
 
-// Test event objects
-var events = [
+// Test transaction objects
+var transactions = [
     {
         id: 1,
         title: 'Electric Bill: $100',
         start: '2014-11-15',
-        cashFlow: -100,
+        amount: -100,
         color: 'red'
     },
     {
         id: 2,
         title: 'Water Bill: $100',
         start: '2014-11-16',
-        cashFlow: -80,
+        amount: -80,
         color: 'red'
     }
 ];
@@ -38,42 +38,42 @@ var events = [
 // Initialize calendar plug-in
 $('#calendar').fullCalendar({
 
-    // Header styling for month view
+    // Header styling
     header: {
         left: 'prev',
         center: 'title',
         right: 'next'
     },
 
-    // Allow dragging events between days
+    // Allow dragging transactions between days
     editable: true,
 
-    // Load events array
-    events: events,
+    // Load transactions array
+    events: transactions,
 
-    // Tooltip displays the date of an event on mouseOver
+    // Tooltip displays the date of a transaction on mouseOver
     eventRender: function(event, element) {
         element.attr('title', event.start.format());
     },
 
-    // Allow event editing and deletion on event click
+    // Allow transaction editing and deletion on transaction click
     eventClick: function(event) {
 
-        // Save event in holder variable for the dialog form
-        selectedEvent = event;
+        // Save transaction in holder variable for the dialog form
+        selected = event;
 
-        // Remove the cashFlow from the title for editing
-        var title = selectedEvent.title;
+        // Remove the amount from the title for editing
+        var title = selected.title;
         var endIndex = title.indexOf(':');
         var transaction = title.slice(0, endIndex);
 
         // Set the default form values
-        $('#title').attr('value', transaction);
-        $('#cashFlow').attr('value', selectedEvent.cashFlow);
+        $('#editTitle').attr('value', transaction);
+        $('#editAmount').attr('value', selected.amount);
 
-        // Open the dialog with tabs
+        // Open dialog with tabs
         $('#editTabs').tabs();
-        $('#editEventDialog').dialog({
+        $('#dialog').dialog({
             modal: true,
             draggable: false,
             resizable: false
@@ -89,141 +89,83 @@ $('#calendar').fullCalendar({
     dayClick: function(date, jsEvent, view) {
         $('#calendar').fullCalendar('gotoDate', date);
         $('#calendar').fullCalendar('changeView', 'basicDay');
-        $('#month').show();
+        $('#monthButton').show();
     }
 });
 
-// Button on basicDay view to return back to month view
-$('#month').on('click', function() {
+// Button on date view to return back to month view
+$('#monthButton').on('click', function() {
     $('#calendar').fullCalendar('changeView', 'month');
-    $('#month').hide();
+    $('#monthButton').hide();
 });
 
-// Edit an event on the calendar
-$('#editEvent').on('click', function() {
-    var title = $('#title').val();
-    var cashFlow = $('#cashFlow').val();
+// Edit a transaction on the calendar
+$('#editButton').on('click', function() {
+    var title = $('#editTitle').val();
+    var amount = $('#editAmount').val();
 
     // Ensure all values are filled in
-    if (title != '' && cashFlow != '') {
-        selectedEvent.title = title + ': $' + cashFlow;
-        selectedEvent.cashFlow = cashFlow;
-        
-        // Set event color based on cash flow
-        if (cashFlow < 0) {
-            selectedEvent.color = 'red';
-        } else {
-            selectedEvent.color = 'green';
-        }
+    if (title != '' && amount != '') {
+        selected.title = title + ': $' + amount;
+        selected.amount = amount;
 
-        // Render new event info
-        $('#calendar').fullCalendar('updateEvent', selectedEvent);
+        // Render new transaction info
+        $('#calendar').fullCalendar('updateEvent', selected);
         
         // Clear variables 
-        selectedEvent = undefined;
-        $('#title').val(undefined);
-        $('#cashFlow').val(undefined);
+        selected = undefined;
+        $('#editTitle').val(undefined);
+        $('#editAmount').val(undefined);
 
         // Close dialog
-        $('#editEventDialog').dialog('close');
+        $('#dialog').dialog('close');
     }
 });
 
-// Remove an event from the calendar
-$('#deleteEvent').on('click', function() {
+// Remove a transaction from the calendar
+$('#deleteButton').on('click', function() {
 
     // Pull event id and remove event
-    var id = selectedEvent.id;
+    var id = selected.id;
     $('#calendar').fullCalendar('removeEvents', id);
 
     // Clear event holder variable and close dialog
-    selectedEvent = undefined;
-    $('#editEventDialog').dialog('close');
+    selected = undefined;
+    $('#dialog').dialog('close');
 });
 
 // Add a single event to the calendar
-$('#addSingleEvent').on('click', function() {
+$('#addButton').on('click', function() {
 
     // Development random id generator
     var id = Math.floor(Math.random()*1001);
 
     // Pull form input values
-    var title = $('#singleTitle').val();
-    var start = $('#singleDate').val();
-    var cashFlow = $('#singleCashFlow').val();
+    var title = $('#addTitle').val();
+    var start = $('#addStart').val();
+    var amount = $('#addAmount').val();
 
     // Ensure all values are filled in
-    if (title != '' && start != '' && cashFlow != '') {
-        
-        // Set event color based on cash flow
-        var color = '';
+    if (title != '' && start != '' && amount != '') {
 
-        if (cashFlow < 0) {
-            color = 'red';
-        } else {
-            color = 'green';
-        }
-
-        // Create single transaction object
-        var singleTransaction = {
+        // Create transaction object
+        var transaction = {
             id: id,
-            title: title + ': $' + cashFlow,
+            title: title + ': $' + amount,
+            amount: amount,
+            frequency: frequency,
             start: start,
-            cashFlow: cashFlow,
-            color: color
+            // end: end
         };
 
-        // Render the event on the calendar
-        $('#calendar').fullCalendar('renderEvent', singleTransaction, true);
-
-        // Clear form values 
-        $('#singleTitle').val(undefined);
-        $('#singleDate').val(undefined);
-        $('#singleCashFlow').val(undefined);
-    }
-});
-
-// Add a recurring event to the calendar
-$('#addRecurringEvent').on('click', function() {
-
-    // Development random id generator
-    var id = Math.floor(Math.random()*1001);
-
-    var frequency = $('#frequency input[name=frequency]:checked').val();
-    var title = $('#recurringTitle').val();
-    var start = $('#recurringStartDate').val();
-    var end = $('#recurringEndDate').val();
-    var cashFlow = $('#recurringCashFlow').val();
-
-    // Ensure all values are filled in
-    if (frequency && title != '' && start != '' && end != '' && cashFlow != '') {
-        
-        // Set event color based on cash flow
-        var color = '';
-
-        if (cashFlow < 0) {
-            color = 'red';
-        } else {
-            color = 'green';
-        }
-
-        // Create recurring transaction object
-        var recurringTransaction = {
-            id: id,
-            title: title + ': $' + cashFlow,
-            start: start,
-            end: end,
-            cashFlow: cashFlow,
-            color: color
-        };
+        console.log(transaction);
 
         // Render the event on the calendar
-        $('#calendar').fullCalendar('renderEvent', recurringTransaction, true);
+        $('#calendar').fullCalendar('renderEvent', transaction, true);
 
         // Clear form values 
-        $('#recurringTitle').val(undefined);
-        $('#recurringStartDate').val(undefined);
-        $('#recurringEndDate').val(undefined);
-        $('#recurringCashFlow').val(undefined);
+        $('#addTitle').val(undefined);
+        $('#addStart').val(undefined);
+        $('#addAmount').val(undefined);
     }
 });
